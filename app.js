691f309e05fd4b9490fa6fa567873b546b17b3d6
3la2i <alaa.ata25@gmail.com>
2024-06-04 00:48:53 +0300

    solved task

---
+++ app.js
@@ -0,0 +1,28 @@
+let name = prompt("Please enter your name");
+let gender = prompt("please enter your gender male/female");
+
+alert("Welcome, " + name + "!")
+
+
+let order = prompt("What would you like to order? Donut, Coffee, Ice Cream, or Bakery?")
+
+switch (order) {
+    case 'donut':
+        alert("You ordered a donut.");
+        break;
+    case 'coffee':
+        alert("You ordered a coffee.");
+        break;
+    case 'ice cream':
+        alert("You ordered an ice cream.");
+        break;
+    case 'bakery':
+        alert("You ordered from the bakery.");
+        break;
+    default:
+        alert("Invalid choice. Please choose from Donut, Coffee, Ice Cream, or Bakery.");
+}
+
+
+let = alert("your order is bieng brebaired")
+
